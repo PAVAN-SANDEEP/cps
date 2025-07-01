c175e58945afafab851a763a00093a8d256f2bc2
PAVAN-SANDEEP <sbspcm143@gmail.com>
2025-07-01 21:18:57 +0530

    Update Contact.tsx

--- team-sandeep-conceptrecommender/src/pages/Contact.tsx
+++ team-sandeep-conceptrecommender/src/pages/Contact.tsx
@@ -1,4 +1,4 @@
-// Created by : @Pentapati V V Satya Pavan Sandeep make changes
+// Created by : @Pentapati V V Satya Pavan Sandeep
 import React, { useState } from 'react';
 import { Brain, Mail, Phone, MapPin, Send, MessageSquare, Clock, ArrowLeft } from 'lucide-react';
 
